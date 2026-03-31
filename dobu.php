691c338126429e6a5fabea1f9d2f2a65a6176203
Christos Drogidis <4207994+ascoos@users.noreply.github.com>
2026-03-31 20:23:50 +0300

    dobu

---
+++ dobu.php
@@ -0,0 +1,61 @@
+<?php 
+/*
+dobu {
+    file:id(`1`),name(`create-my-project`) {
+        ascoos {
+            logo {`
+                  __ _  ___  ___ ___   ___   ___     ___   ___
+                 / _V |/  / / __/ _ \ / _ \ /  /    / _ \ /  /
+                | (_| |\  \| (_| (_) | (_) |\  \   | (_) |\  \
+                 \__,_|/__/ \___\___/ \___/ /__/    \___/ /__/
+            `},
+            name {`ASCOOS OS`},
+            version {`1.0.0`},
+            category {`Web OS`},
+            subcategory {`Web5 / WebAI`},
+            description {`A Web 5.0 and Web AI Kernel for decentralized web and IoT applications`},
+            creator {`Drogidis Christos`},
+            site {`https://www.ascoos.com`},
+            issues {`https://issues.ascoos.com`},
+            support {`support@ascoos.com`},
+            license {`[Commercial] http://docs.ascoos.com/lics/ascoos/AGL.html`},
+            copyright {`Copyright (c) 2007 - 2026, AlexSoft Software.`}
+        },
+        project {
+            package:langs {
+                en {`DoBu code sample`},
+                el {`Δείγμα κώδικα DoBu`}
+            },
+            subpackage:langs {
+                en {`DoBu code sample for classes and methods`},
+                el {`Δείγμα κώδικα DoBu για κλάσεις και μεθόδους`}
+            },
+            category:langs {
+                en {`Examples`},
+                el {`Παραδείγματα`}
+            },
+            subcategory:langs {
+                en {`DoBu for classes`},
+                el {`DoBu για κλάσεις`}
+            },
+            source {`examples/create-my-project.php`},
+            description:langs {
+                en {`This file is a complete example of docblock writing and DoBu structure.`},
+                el {`Το αρχείο αυτό αποτελεί ένα ολοκληρωμένο παράδειγμα σύνταξης docblock, δομής DoBu`}
+            },
+            fileNo {`1`},
+            version {`1.0.0`},
+            build {`1`},
+            created {`2026-02-12 09:25:03`},
+            updated {`2026-02-12 09:51:43`},
+            author {`Author Name`},
+            authorSite {`https://www.example.com`},
+            support {`support@example.com`},
+            license {`AGL`},
+            since {`1.0.0`},
+            sincePHP {`8.4.0`}
+        }
+    }
+}
+*/
+?>
